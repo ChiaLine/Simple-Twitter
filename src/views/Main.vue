c558a52719658a0234e74f97fee8b57010e2c449
egg <egg810207@gmail.com>
2022-02-23 02:26:14 +0800

    add PopularUsers

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Main</h1>
+  <div class="main">
+    <h1>Main</h1>
+  </div>
 </template>
+
+<style scoped>
+  .main {
+    height: 1202px;
+    width: 600px;
+    border-radius: 5px;
+    border: 1px solid #E6ECF0;
+    /* background-color: rgb(238, 236, 234); */
+  }
+</style>
