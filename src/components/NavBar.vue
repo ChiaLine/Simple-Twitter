0ad5fa530da30e8d0520e77517e70598b6421e98
egg <egg810207@gmail.com>
2022-02-23 17:22:14 +0800

    Update NavBar end

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,43 +6,110 @@
       </a>
     </div>
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#/User">
-          <img class="navbar-icon home" src="https://i.imgur.com/fBVbYGb.png" />
-          <span>首頁</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#/User/UserSelf">
-          <img
-            class="navbar-icon user-self"
-            src="https://i.imgur.com/feES0o7.png"
-          />
-          <span>個人資料</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#/Setting">
-          <img
-            class="navbar-icon setting"
-            src="https://i.imgur.com/rottgfT.png"
-          />
-          <span>設定</span>
-        </a>
-      </li>
-      <li class="nav-item sign-out-bottom">
-        <a class="nav-link" href="#/Login">
-          <img
-            class="navbar-icon sign-out"
-            src="https://i.imgur.com/trtoBHw.png"
-          />
-          <span>登出</span>
-        </a>
+      <li class="nav-item" 
+      v-for="item in navItems" 
+      :key="item.id"
+      @click.stop.prevent="toggleActive(item.id)"
+      >
+        <router-link
+          class="nav-link"
+          :to="{
+            name: item.to
+          }"
+          :class="{ active: item.active }"
+        >
+          <img class="navbar-icon home" :src="item.iconActive" v-if="item.active"/>
+          <img class="navbar-icon home" :src="item.icon" v-else/>
+          <span>{{ item.title }}</span>
+        </router-link>
       </li>
     </ul>
     <button class="navbar-button">推文</button>
   </nav>
 </template>
+
+<script>
+const dummyData = {
+  navItems: [
+    {
+      id: 1,
+      title: "首頁",
+      icon: 'https://i.imgur.com/fBVbYGb.png',
+      iconActive: 'https://i.imgur.com/YpAk1au.png',
+      to: 'Main',
+      active: false,
+    },
+    {
+      id: 2,
+      title: "個人資料",
+      icon: 'https://i.imgur.com/feES0o7.png',
+      iconActive: 'https://i.imgur.com/rGhaeOT.png',
+      to: 'UserSelf',
+      active: false,
+    },
+    {
+      id: 3,
+      title: "設定",
+      icon: 'https://i.imgur.com/rottgfT.png',
+      iconActive: 'https://i.imgur.com/WMyBdJ2.png',
+      to: 'Setting',
+      active: false,
+    },
+    {
+      id: 4,
+      title: "登出",
+      icon: 'https://i.imgur.com/trtoBHw.png',
+      to: 'Login',
+      active: false,
+    },
+  ],
+};
+
+export default {
+  data() {
+    return {
+      navItems: [],
+    }
+  },
+  created() {
+    this.fetchNavItems();
+    // console.log(this.$route.name)
+  },
+  methods: {
+    fetchNavItems() {
+      const { navItems } = dummyData
+      this.navItems = navItems.map( item => {
+        if (item.to === this.$route.name) {
+          return {
+            ...item,
+            active: true
+          }
+        } else {
+          return {
+            ...item
+          }
+        }
+      })
+    },
+    toggleActive(itemId) {
+      // console.log('toggleActive', itemId)
+      this.navItems = this.navItems.map( item => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            active: true
+          }
+        } else {
+          return {
+            ...item,
+            active: false
+          }
+        }
+      })
+    },
+  }
+}
+</script>
 
 <style lang="scss">
 @import "../assets/scss/navbar.scss";
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Setting.vue</h1>
+  <div class="w-100 h-100 d-flex justify-content-end">
+    <NavBar />
+    <div class="right">
+      <h5 class="title p-3">帳戶設定</h5>
+      <h3 class="p-3">放修改表單區</h3>
+    </div>
+  </div>
 </template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+
+export default {
+  name: 'Setting',
+  components: {
+    NavBar,
+  },
+}
+</script>
+
+<style scoped>
+  .right {
+    width: 75%;
+    border-radius: 5px;
+    border-left: 1px solid #E6ECF0;
+    /* background-color: rgb(238, 236, 234); */
+  }
+  .title {
+    border-bottom: 1px solid #E6ECF0;
+  }
+</style>
