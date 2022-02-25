bfe395f7b10f22951b639191fd86569177141ed4
Christine <twt249@gmail.com>
2022-02-25 17:06:23 +0800

    create replyList page

--- src/views/ReplyList.vue
+++ src/views/ReplyList.vue
@@ -1,3 +1,29 @@
+// src/views/ReplyList.vue
 <template>
-  <h1>ReplyList</h1>
+  <div class="replyList">
+    <ReplyListTweet />
+    <ReplyListCards />
+  </div>
 </template>
+
+<script>
+import ReplyListTweet from "./../components/ReplyListTweet";
+import ReplyListCards from "./../components/ReplyListCards";
+
+export default {
+  components: {
+    ReplyListTweet,
+    ReplyListCards,
+  },
+};
+</script>
+
+
+<style scoped>
+.replyList {
+  width: 600px;
+  border-radius: 5px;
+  border: 1px solid #e6ecf0;
+  /* background-color: rgb(238, 236, 234); */
+}
+</style>
