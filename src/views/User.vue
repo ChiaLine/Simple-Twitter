3505ab53f6180e17d32690c2101ff4c003554bde
egg <egg810207@gmail.com>
2022-02-25 20:02:14 +0800

    新增點擊按鈕後會秀出Modal視窗串接完成

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="user w-100 h-100 d-flex justify-content-center">
-    <NavBar />
-    <router-view />
+    <NavBar @after-show-modal="afterShowModal"/>
+    <router-view @after-show-modal="afterShowModal" />
     <PopularUsers />
+    <TweetModal v-if="showModal" @after-hide-modal="afterHideModal"/>
   </div>
 </template>
 
 <script>
 import NavBar from '../components/NavBar.vue'
 import PopularUsers from '../components/PopularUsers.vue'
+import TweetModal from '../components/TweetModal.vue'
+
 
 export default {
   name: "User",
   components: {
     NavBar,
     PopularUsers,
-  }
+    TweetModal,
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    afterShowModal() {
+      console.log("afterShowModal--user");
+      this.showModal = true
+    },
+    afterHideModal() {
+      console.log("afterHideModal--user");
+      this.showModal = false
+    }
+  },
 }
 </script>
 
