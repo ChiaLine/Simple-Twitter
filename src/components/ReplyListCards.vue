c6455dea7be4fd537e464b1192ce5baa26b76caf
Christine <twt249@gmail.com>
2022-02-26 17:16:15 +0800

    replyList api 1st version: 1.GET /api/tweets/:TweetId 2.GET /api/tweets/:TweetId/replies

--- src/components/ReplyListCards.vue
+++ src/components/ReplyListCards.vue
@@ -1,24 +1,41 @@
 // src/components/ReplyListCards.vue
 // ReplyList.vue使用
 <template>
-  <div class="w-100">
-    <div class="wrapper d-flex">
-      <img src="https://i.imgur.com/suYPL6j.png" alt="" class="avatar mt-3" />
-      <div class="info py-3">
-        <div class="user d-flex mb-1">
-          <h5 class="account mr-1">Mary Jane</h5>
-          <h5 class="grey-font mr-2">@mjjjjj</h5>
-          <h5 class="grey-font">13小時</h5>
-        </div>
-        <div class="ReplyUser d-flex mb-1">
-          <p class="grey-font mr-1">回覆</p>
-          <h5 class="orange-font">@apple</h5>
-        </div>
-        <p class="replyContent">Great~</p>
+  <div class="wrapper d-flex">
+    <img :src="card.repliedUser.avatar" alt="" class="avatar mt-3" />
+    <div class="info py-3">
+      <div class="user d-flex mb-1">
+        <h5 class="account mr-1">{{ card.repliedUser.name }}</h5>
+        <h5 class="grey-font mr-2">@{{ card.repliedUser.account }}</h5>
+        <h5 class="grey-font">{{ card.updatedAt | fromNow }}</h5>
       </div>
+      <div class="ReplyUser d-flex mb-1">
+        <p class="grey-font mr-1">回覆</p>
+        <h5 class="orange-font">@{{ tweetedUser }}</h5>
+      </div>
+      <p class="replyContent">{{ card.comment }}</p>
     </div>
   </div>
 </template>
+
+<script>
+import { fromNowFilter } from "./../utils/mixins";
+
+export default {
+  mixins: [fromNowFilter],
+  props: {
+    card: {
+      type: Object,
+      required: true,
+    },
+    tweetedUser: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
 
 <style scoped lang="scss">
 @import "../assets/scss/reply.scss";
