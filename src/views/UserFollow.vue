0f1bddff6b58ef26b6b7f7666d6d57316aadf9de
Kanmurio406 <jenny2311@gmail.com>
2022-02-27 17:14:24 +0800

    add UserFollow basic template and styles setting

--- src/views/UserFollow.vue
+++ src/views/UserFollow.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>UserFollow</h1>
+  <div class="user-follow">
+    <nav>
+      <!-- 回上一頁區塊 -->
+      <!-- 要再將連結修改成回到原使用者UserSelf頁面，需要Id -->
+      <router-link to="/User/UserSelf" class="wrapper d-flex">
+        <img class="arrow" src="https://i.imgur.com/3y7W3fG.png" alt="" />
+        <div class="m-2">
+          <h5 class="userName">John Doe</h5>
+          <h5 class="tweetNumber">25貼文</h5>
+        </div>
+      </router-link>
+      <!-- 卡片切換導覽列 -->
+      <div class="card-buttons">
+        <button class="card-button">
+          <span>跟隨者</span>
+        </button>
+        <button class="card-button">
+          <span>正在跟隨</span>
+        </button>
+      </div>
+    </nav>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/user-follow.scss";
+</style>
