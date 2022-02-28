83706dd50c9be4479fdf31a0200886ce7bbff2af
egg <egg810207@gmail.com>
2022-02-28 22:10:48 +0800

    新增API串接刪除推文功能，按鈕重複點選禁按功能

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -5,6 +5,9 @@ export default {
   getTweetsList() {
     return apiHelper.get(`/admin/tweets`, { headers: { Authorization: `Bearer ${getAdminToken()}` } })
   },
+  deleteTweet(tweetId) {
+    return apiHelper.delete(`/admin/tweets/${tweetId}`, { headers: { Authorization: `Bearer ${getAdminToken()}` } })
+  },
   getUsersList() {
     return apiHelper.get(`/admin/users`, { headers: { Authorization: `Bearer ${getAdminToken()}` } })
   },
