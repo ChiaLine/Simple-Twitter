d37bebeb48f0b481c75c10f97b12b8b1f2899c7b
egg <egg810207@gmail.com>
2022-03-03 00:38:10 +0800

    新增後台登出功能，未登入狀態可成功轉址404

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,22 @@ import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import NotFound from '../views/NotFound.vue'
 import User from '../views/User.vue'
+// 載入 state
 import store from './../store'
 
 Vue.use(VueRouter)
+
+// 沒有驗證的管理者，即使知道網址、也應該被轉址到 404 頁面
+const authorizeIsAdmin = (to, from, next) => {
+  const adminToken = store.state.adminToken
+  // 若帳號沒有 token 的驗證就轉址到 404 頁面
+  if (adminToken === null) {
+    next('/notfound')
+    return
+  }
+
+  next()
+}
 
 const router = new VueRouter({
   routes: [
@@ -72,16 +85,20 @@ const router = new VueRouter({
       name: 'Admin',
       redirect: '/Admin/MainList',
       component: () => import('../views/Admin.vue'),
+      // 沒有權限的使用者來說，即使他知道網址、也應該被轉址到 404 頁面
+      beforeEnter: authorizeIsAdmin,
       children: [
         {
           path: 'MainList',
           name: 'MainList',
           component: () => import('../views/AdminMainList.vue'),
+          beforeEnter: authorizeIsAdmin
         },
         {
           path: 'UserList',
           name: 'UserList',
           component: () => import('../views/AdminUserList.vue'),
+          beforeEnter: authorizeIsAdmin
         },
       ]
     },
@@ -102,6 +119,8 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   // 使用 dispatch 呼叫 Vuex 內的 actions
   store.dispatch('fetchCurrentUser')
+  // 新增後台 token 驗證
+  store.commit("setAdminUser");
   next()
 })
 
