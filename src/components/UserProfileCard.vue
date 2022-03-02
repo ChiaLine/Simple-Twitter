23a4686b1075da2dd12741ad47cef4437d9ccdd9
Christine <r05626001@ntu.edu.tw>
2022-03-02 12:08:06 +0800

    adjust style

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -1,3 +1,4 @@
+// src/components/UserProfileCard.vue
 <template>
   <div class="wrapper">
     <!-- 要再將連結修改成上一頁 -->
@@ -122,101 +123,5 @@ export default {
 
 
 <style scoped lang="scss">
-@import "../assets/scss/_color.scss";
-@import "../assets/scss/_extends.scss";
-$border-style: 1px solid #e6ecf0;
-
-.wrapper {
-  max-width: 600px;
-  border-right: $border-style;
-  border-left: $border-style;
-}
-.arrow {
-  width: 17px;
-  height: 14px;
-  margin: 1.25rem;
-}
-.returnTweet {
-  @extend %title-text;
-}
-.tweetNumber {
-  @extend %tag-text;
-  font-size: 13px;
-  line-height: 19px;
-}
-.cover {
-  max-width: 600px;
-  max-height: 200px;
-  border-radius: 0%;
-}
-.avatar {
-  width: 140px;
-  height: 140px;
-  transform: translateY(-50%);
-  border: 4px solid #ffffff;
-}
-.editProfile {
-  right: 0.1rem;
-  transform: translate(-7%, -155%);
-  width: 130px;
-  height: 35px;
-  @extend %second-title-text;
-  line-height: 15px;
-  color: $button-orange;
-  border: 1px solid $button-orange;
-  border-radius: 30px;
-  margin-left: 0.4rem;
-}
-.iconWrapper {
-  transform: translate(-7%, -155%);
-  width: 35px;
-  height: 35px;
-  color: $button-orange;
-  border: 1px solid $button-orange;
-  border-radius: 30px;
-  margin-right: 0.625rem;
-  .iconMail {
-    width: 17.5px;
-    height: 15.75px;
-    border-radius: 0px;
-  }
-  .iconNotify {
-    width: 20.92px;
-    height: 19.16px;
-    border-radius: 0px;
-  }
-}
-.profile {
-  top: 4.3rem;
-  margin-bottom: 7rem;
-}
-.followOther {
-  right: 0.1rem;
-}
-.name {
-  @extend %title-text;
-}
-.account {
-  @extend %tag-text;
-}
-.note {
-  margin: 10px 0;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 14px;
-  line-height: 20px;
-}
-.number {
-  @extend %content-text;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 20px;
-}
-.text {
-  @extend %content-text;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 20px;
-  color: #657786;
-}
+@import "../assets/scss/user-profile-card.scss";
 </style>
