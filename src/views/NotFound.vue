aeddc1d2f440d7a2a7bf5633052494993ce3988a
egg <egg810207@gmail.com>
2022-03-03 00:37:12 +0800

    新增404頁面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1> 404 NotFound.vue</h1>
+  <div class="not-found">
+    <img class="not-found-img" src="https://i.imgur.com/XK8i1m0.png">
+  </div>
 </template>
+
+<style scoped>
+.not-found {
+  width: 900px;
+  height: 650px;
+  margin: 100px auto;
+}
+.not-found-img {
+  border-radius: 0;
+}
+</style>
