b2a31d8aa3f6c4c1b9b82de218edb547e5a44d96
egg <egg810207@gmail.com>
2022-03-02 19:39:25 +0800

    新增共用元件內的功能成功API串接、成功接到父層傳遞的user資料

--- src/components/UserTweetListCard.vue
+++ src/components/UserTweetListCard.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
+    <!-- 按鈕區 -->
     <div class="self-buttons">
       <button class="self-button"
-      @click.stop.prevent="fetchUserTweets(currentUser.id)"
+      @click.stop.prevent="fetchUserTweets(user.id)"
       >推文</button>
       <button class="self-button"
-      @click.stop.prevent="fetchUserReplyTweets(currentUser.id)"
+      @click.stop.prevent="fetchUserReplyTweets(user.id)"
       >推文與回覆</button>
       <button class="self-button"
-      @click.stop.prevent="fetchUserLikeTweets(currentUser.id)"
+      @click.stop.prevent="fetchUserLikeTweets(user.id)"
       >喜歡的內容</button>
     </div>
+    <!-- 自己的推文列表 -->
     <div class="tweet-cards" v-if="toggleButton === 'tweet'">
       <div class="tweet-card" v-for="tweet in userTweets" :key="tweet.id">
         <div class="tweet-card-top">
@@ -18,7 +20,7 @@
             <div>
               <img
                 class="tweet-card-img"
-                :src="currentUser.avatar | emptyImage"
+                :src="user.avatar | emptyImage"
               />
             </div>
           </div>
@@ -27,10 +29,10 @@
             @click.stop.prevent="toReplyList()"
           >
             <div >
-              <span class="tweet-card-name">{{ currentUser.name }}</span>
+              <span class="tweet-card-name">{{ user.name }}</span>
               <span class="tweet-card-account">
-                @{{ currentUser.account }}・{{
-                  currentUser.createdAt | formatDate
+                @{{ user.account }}・{{
+                  tweet.createdAt | formatDate
                 }}
               </span>
               <p class="tweet-card-description">
@@ -42,7 +44,7 @@
         <div class="tweet-card-icons">
           <button
             class="tweet-reply-icon"
-            @click.stop.prevent="afterShowReplyModal(currentUser.id)"
+            @click.stop.prevent="afterShowReplyModal(tweet.id)"
           >
             <img class="icon-reply" :src="iconReply" />
             <span>{{ tweet.totalReplies }}</span>
@@ -51,6 +53,7 @@
             class="tweet-like-icon"
             v-if="tweet.isLiked"
             :disabled="isProcessing"
+            @click.stop.prevent="deleteTweetLike(tweet.id)"
           >
             <img class="icon-like" :src="iconIsLike" />
             <span>{{ tweet.totalLikes }}</span>
@@ -59,6 +62,7 @@
             class="tweet-like-icon"
             v-else
             :disabled="isProcessing"
+            @click.stop.prevent=" addTweetCardLike(tweet.id)"
           >
             <img class="icon-like" :src="iconUnLike" />
             <span>{{ tweet.totalLikes }}</span>
@@ -66,7 +70,7 @@
         </div>
       </div>
     </div>
-
+    <!-- 自己的推文回覆列表 -->
     <div class="tweet-cards" v-else-if="toggleButton === 'reply'">
       <div class="tweet-card" v-for="tweet in userRepliedTweets" :key="tweet.id">
         <div class="tweet-card-top">
@@ -74,7 +78,7 @@
             <div>
               <img
                 class="tweet-card-img"
-                :src="currentUser.avatar | emptyImage"
+                :src="user.avatar | emptyImage"
               />
             </div>
           </div>
@@ -83,10 +87,10 @@
             @click.stop.prevent="toReplyList"
           >
             <div >
-              <span class="tweet-card-name">{{ currentUser.name }}</span>
+              <span class="tweet-card-name">{{ user.name }}</span>
               <span class="tweet-card-account">
-                @{{ currentUser.account }}・{{
-                  currentUser.createdAt | formatDate
+                @{{ user.account }}・{{
+                  tweet.createdAt | formatDate
                 }}
               </span>
               <div class="tweet-card-reply">
@@ -94,14 +98,14 @@
                 <span class="card-reply-name">@{{ tweet.repliedTweet.tweetedUser.account }}</span>
               </div>
               <p class="tweet-card-description">
-                {{ tweet.repliedTweet.description }}
+                {{ tweet.comment }}
               </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-
+    <!-- 喜歡的列表 -->
     <div class="tweet-cards" v-else-if="toggleButton === 'like'">
       <div class="tweet-card" v-for="tweet in userLikeTweets" :key="tweet.id">
         <div class="tweet-card-top">
@@ -109,7 +113,7 @@
             <div>
               <img
                 class="tweet-card-img"
-                :src="tweet.avatar | emptyImage"
+                :src="tweet.likedTweet.tweetedUser.avatar | emptyImage"
               />
             </div>
           </div>
@@ -118,10 +122,10 @@
             @click.stop.prevent="toReplyList"
           >
             <div >
-              <span class="tweet-card-name">{{ tweet.id }}</span>
+              <span class="tweet-card-name">{{ tweet.likedTweet.tweetedUser.name }}</span>
               <span class="tweet-card-account">
-                @{{ tweet.id }}・{{
-                  tweet.id | formatDate
+                @{{ tweet.likedTweet.tweetedUser.account }}・{{
+                  tweet.likedTweet.tweetedUser.createdAt | formatDate
                 }}
               </span>
               <p class="tweet-card-description">
@@ -133,15 +137,16 @@
         <div class="tweet-card-icons">
           <button
             class="tweet-reply-icon"
-            @click.stop.prevent="afterShowReplyModal(currentUser.id)"
+            @click.stop.prevent="afterShowReplyModal(tweet.id)"
           >
             <img class="icon-reply" :src="iconReply" />
             <span>{{ tweet.likedTweet.totalReplies }}</span>
           </button>
           <button
             class="tweet-like-icon"
-            v-if="tweet.isLiked"
+            v-if="tweet.likedTweet.isLiked"
             :disabled="isProcessing"
+            @click.stop.prevent="deleteTweetLike(tweet.likedTweet.id)"
           >
             <img class="icon-like" :src="iconIsLike" />
             <span>{{ tweet.likedTweet.totalLikes }}</span>
@@ -150,6 +155,7 @@
             class="tweet-like-icon"
             v-else
             :disabled="isProcessing"
+            @click.stop.prevent=" addTweetCardLike(tweet.likedTweet.id)"
           >
             <img class="icon-like" :src="iconUnLike" />
             <span>{{ tweet.likedTweet.totalLikes }}</span>
@@ -162,43 +168,85 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
 import { emptyImageFilter } from "../utils/mixins";
 import { formatDateFilter } from "./../utils/mixins";
 import userTweetsAPI from "../apis/userTweets";
+import tweetAPI from "../apis/tweetCards";
 import { Toast } from "../utils/helpers";
 
 export default {
   name: 'UserTweetListCard',
+  props: {
+    initialUser: {
+      type: Object,
+      required: true,
+    },
+  },
   mixins: [emptyImageFilter, formatDateFilter],
   data() {
     return {
+      user: {
+        id: -1,
+        account: "",
+        email: "",
+        name: "",
+        avatar: "",
+        cover: null,
+        introduction: "",
+        role: "",
+        totalTweets: 0,
+        totalFollowings: 0,
+        totalFollowers: 0,
+        totalLiked: 0,
+        createdAt: "",
+        updatedAt: "",
+      },
+      isCurrentUser: "",
       userTweets: [],
       userRepliedTweets: [],
       userLikeTweets: [],
-      toggleButton: 'tweet',
       iconUnLike: "https://i.imgur.com/fWY8yOj.png",
       iconIsLike: "https://i.imgur.com/LQTMNI0.png",
       iconReply: "https://i.imgur.com/EwJRYkP.png",
+      toggleButton: 'tweet',
       isProcessing: false,
+      currentPage: '',
     };
   },
-  computed: {
-    ...mapState(["currentUser"]),
-  },
   watch: {
-    currentUser() {
-      console.log('222', this.currentUser.id)
-      this.fetchUserTweets(this.currentUser.id);
-    }
+    initialUser(newValue) {
+      this.user = {
+        ...this.user,
+        ...newValue,
+      };
+      this.fetchUserTweets(this.user.id)
+    },
+  },
+  created() {
+    this.checkCurrentPage();
   },
   methods: {
+    checkCurrentPage() {
+      this.currentPage = this.$route.name;
+      if (this.currentPage === "UserSelf") {
+        this.isCurrentUser = true;
+      } else if (this.currentPage === "UserOther") {
+        this.isCurrentUser = false;
+      }
+    },
     async fetchUserTweets(userId) {
       try {
         console.log('fetchUserTweets', userId)
         const { data } = await userTweetsAPI.getUserTweets(userId);
-        // console.log(data)
         this.userTweets = data;
+        
+        if (!this.userTweets.length) {
+          Toast.fire({
+            icon: "error",
+            title: '沒有推文的內容',
+          });
+        }
+
         this.toggleButton = 'tweet';
       } catch (error) {
         console.error(error);
@@ -212,10 +260,15 @@ export default {
       try {
         console.log('ReplyTweets', userId)
         const { data } = await userTweetsAPI.getUserRepliedTweets(userId);
-        console.log(data)
         this.userRepliedTweets = data;
-        this.toggleButton = 'reply';
 
+        if (!this.userRepliedTweets.length) {
+          Toast.fire({
+            icon: "error",
+            title: '沒有回覆推文的內容',
+          });
+        }
+        this.toggleButton = 'reply';
       } catch (error) {
         console.error(error);
         Toast.fire({
@@ -228,10 +281,16 @@ export default {
       try {
         console.log('UserLikeTweets', userId)
         const { data } = await userTweetsAPI.getUserLikeTweets(userId);
-        console.log(data)
         this.userLikeTweets = data;
-        this.toggleButton = 'like';
 
+        if (!this.userLikeTweets.length) {
+          Toast.fire({
+            icon: "error",
+            title: '沒有喜歡的內容',
+          });
+        }
+
+        this.toggleButton = 'like';
       } catch (error) {
         console.error(error);
         Toast.fire({
@@ -240,13 +299,57 @@ export default {
         });
       }
     },
-    afterShowReplyModal(replyUserId) {
-      console.log("ShowReplyModal----UserTweetListCard", replyUserId);
-      this.$emit("after-show-reply-modal",replyUserId);
+    async addTweetCardLike(id) {
+      try {
+        this.isProcessing = true;
+        await tweetAPI.addTweetLike(id);
+
+        if (this.toggleButton === "tweet") {
+          await this.fetchUserTweets(this.user.id);
+        } else if (this.toggleButton === "like") {
+          await this.fetchUserLikeTweets(this.user.id);
+        }
+
+        this.isProcessing = false;
+      } catch (error) {
+        this.isProcessing = false;
+        Toast.fire({
+          icon: "error",
+          title: error.response.data.message,
+        });
+      }
+    },
+    async deleteTweetLike(id) {
+      try {
+        this.isProcessing = true;
+        await tweetAPI.deleteTweetLike(id);
+        
+        if (this.currentPage === "UserSelf") {
+          await this.fetchUserLikeTweets(this.user.id);
+        } else if (this.currentPage === "UserOther") {
+          if (this.toggleButton === "tweet") {
+            await this.fetchUserTweets(this.user.id);
+          } else if (this.toggleButton === "like") {
+            await this.fetchUserLikeTweets(this.user.id);
+          }
+        }
+
+        this.isProcessing = false;
+      } catch (error) {
+        this.isProcessing = false;
+        Toast.fire({
+          icon: "error",
+          title: error.response.data.message,
+        });
+      }
+    },
+    afterShowReplyModal(replyTweetId) {
+      console.log("ShowReplyModal----UserTweetListCard", replyTweetId);
+      this.$emit("after-show-reply-modal",replyTweetId);
     },
     toReplyList() {
       // 點選區塊後轉址到 未處理
-      console.log('toReplyList')
+      console.log('to轉址到未處理???')
     },
   },
 }
@@ -261,7 +364,6 @@ export default {
   border-left: 1px solid #e6ecf0;
   border-bottom: 1px solid #e6ecf0;
 }
-
 .self-button {
   margin-left: 55px;
   font-weight: 500;
@@ -269,7 +371,6 @@ export default {
 }
 .card-reply-title {
   color: #657786;
-  // font-weight: 500;
 }
 .card-reply-name{
   color: #FF6600;
