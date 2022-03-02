99b8a0444aab3196ed9aca4412843ec8ff0e28c9
Kuan-Yu Chou <jenny2311@gmail.com>
2022-03-02 17:11:05 +0800

    UserFollowAPI: 新增由params取得userId，並請求使用者資料

--- src/views/UserFollow.vue
+++ src/views/UserFollow.vue
@@ -6,17 +6,25 @@
       <router-link to="/User/UserSelf" class="wrapper d-flex">
         <img class="arrow" src="https://i.imgur.com/3y7W3fG.png" alt="" />
         <div class="m-2">
-          <h5 class="userName">John Doe</h5>
-          <h5 class="tweetNumber">25貼文</h5>
+          <h5 class="userName">{{user.name}}</h5>
+          <h5 class="tweetNumber">{{user.totalTweets}}</h5>
         </div>
       </router-link>
       <!-- 卡片切換導覽列 -->
       <div class="card-buttons d-flex">
         <div v-for="type in dataType" :key="type.id">
-          <button v-if="type.id === dataId" @click.stop.prevent="setDataId(type.id)" class="card-button chosen">
+          <button
+            v-if="type.id === dataId"
+            @click.stop.prevent="setDataId(type.id)"
+            class="card-button chosen"
+          >
             {{ type.buttonName }}
           </button>
-          <button v-else @click.stop.prevent="setDataId(type.id)" class="card-button normal">
+          <button
+            v-else
+            @click.stop.prevent="setDataId(type.id)"
+            class="card-button normal"
+          >
             {{ type.buttonName }}
           </button>
         </div>
@@ -29,6 +37,8 @@
 
 <script>
 import FollowCards from "../components/FollowCards.vue";
+import userFollowAPI from "./../apis/userFollow";
+import { Toast } from "./../utils/helpers";
 
 export default {
   name: "UserFollow",
@@ -48,11 +58,36 @@ export default {
           buttonName: "正在跟隨",
         },
       ],
+      user: {
+        id: -1,
+        account: "dummyUser",
+        name: "dummyUser",
+        totalTweets: 0,
+      },
     };
+  },
+  created() {
+    const { id: userId } = this.$route.params;
+    console.log(userId);
+    this.fetchUser(userId)
   },
   methods: {
     setDataId(id) {
       this.dataId = id;
+    },
+    async fetchUser(userId) {
+      try {
+        let response = await userFollowAPI.getUserProfile(userId);
+        const { data } = response;
+        console.log(data);
+        this.user = {...data}
+      } catch (e) {
+        console.log(e.response.data.message)
+        Toast.fire({
+          icon: "warning",
+          title: "無法取得使用者資料",
+        });
+      }
     },
   },
 };
